28ae99b643c66678d2f87668a8077d9696e96eeb
Enrique Escribano Reimúndez <72576090+EnriqueEscribano@users.noreply.github.com>
2021-03-10 19:03:03 +0100

    Movimiento1

---
+++ Enrique Escribano/Assets/Scripts/MoverCaja.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverCaja : MonoBehaviour
+{
+
+    private float speed = 100f; 
+
+    private string currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoverNave();
+
+    }
+
+    void MoverNave()
+    {
+        float PosX = transform.position.x;
+        float posZ = transform.position.z;
+        float desplX = Input.GetAxis("Horizontal");
+
+        //Restringir movimiento horizontal
+        if ((PosX < 100 || desplX < 0) && (PosX > 100 || desplX > 0))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
+        }
+
+
+        // las || significan or.
+    }
+}
