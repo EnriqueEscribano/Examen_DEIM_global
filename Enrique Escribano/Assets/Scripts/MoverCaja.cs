15b15ae477ca7c936004162f34d0b6c03179f22f
Enrique Escribano Reimúndez <72576090+EnriqueEscribano@users.noreply.github.com>
2021-03-10 19:21:36 +0100

    Incorporar eje z

--- Enrique Escribano/Assets/Scripts/MoverCaja.cs
+++ Enrique Escribano/Assets/Scripts/MoverCaja.cs
@@ -25,8 +25,9 @@ public class MoverCaja : MonoBehaviour
     void MoverNave()
     {
         float PosX = transform.position.x;
-        float posZ = transform.position.z;
+        float PosZ = transform.position.z;
         float desplX = Input.GetAxis("Horizontal");
+        float desplZ = Input.GetAxis("Vertical");
 
         //Restringir movimiento horizontal
         if ((PosX < 100 || desplX < 0) && (PosX > 100 || desplX > 0))
@@ -34,7 +35,16 @@ public class MoverCaja : MonoBehaviour
             transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
         }
 
+        //Restringir movimiento eje Z
+       
+        if ((PosZ < 100 || desplZ < 0) && (PosZ > 100 || desplZ > 0))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * desplZ);
+        }
 
+        
+        
+        
         // las || significan or.
     }
 }
